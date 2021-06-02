c031478731cddfc542478cd7fcb7fa71700d4006
pascal <kongwutianwen@163.com>
2021-06-02 03:40:27 +0000

    feat: add new questions guilde

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -14,9 +14,10 @@ module.exports = {
       {
         text: '教程',
         items: [
+            { text: 'JS入门导读', link: '/jsGuide/1' },
+            { text: 'JS高级导读', link: '/dd/1-3' },
           { text: '全栈入门', link: '/static/1_1' },
           { text: 'JS入门', link: '/js/1' },
-          { text: 'JS高级导读', link: '/dd/1-3' },
           { text: '前端进阶', link: '/fe/vue' },
         ]
       },
@@ -47,6 +48,7 @@ module.exports = {
     sidebar: {
       '/interview/': genSidebarConfig('面试宝典', ['1', '2', '3', '4', '5', '6', '7', '8']),
       '/fe/': genSidebarConfig('大前端教程', ['vue', 'VueRouter', 'vuex',  'linux', 'vscode', 'node', 'code', 'suanfa', 'ngnix', 'pm2', 'ssh']),
+      '/jsGuide/': genSidebarConfig('JS入门导读', ['1', '2', '3', '4', '5', '6', '7', '8', '9']),
       '/js/': genSidebarConfig('JS入门', ['1', '2', '3', '4', '5', '6', '7', '8']),
       '/dd/': genSidebarConfig('导读', ['1-3', '4-6', '7-9', '10-12', '13-15', '16-18', '19-21', '22-25']),
       '/static/': genSidebarConfig('全栈入门',  ['1_1', '1_2', '1_3', '2_1', '2_2', '3_1', '3_2', '3_3', '4_1', '4_2', '4_3', '5_1', '5_2', '5_3', '6_1','6_2', '6_3']),
