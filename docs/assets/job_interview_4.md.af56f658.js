88d5ee9d6062853e12a8640cd355c0777d114acb
kongwutw <chentianwen@yxqiche.com>
2022-02-21 06:30:33 +0800

    deploy

---
+++ docs/assets/job_interview_4.md.af56f658.js
@@ -0,0 +1,184 @@
+import{_ as e,c as n,o as r,a as t}from"./app.65637521.js";const f='{"title":"\u7B97\u6CD5","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5B9E\u73B0bind","slug":"\u5B9E\u73B0bind"},{"level":2,"title":"\u5B9E\u73B0\u6DF1\u62F7\u8D1D","slug":"\u5B9E\u73B0\u6DF1\u62F7\u8D1D"},{"level":2,"title":"\u4E0D\u9700\u8981\u501F\u52A9\u7B2C\u4E09\u4E2A\u4E34\u65F6\u53D8\u91CF\uFF0C\u5B9E\u73B0\u4E24\u4E2A\u53D8\u91CF\u7684\u4EA4\u6362","slug":"\u4E0D\u9700\u8981\u501F\u52A9\u7B2C\u4E09\u4E2A\u4E34\u65F6\u53D8\u91CF\uFF0C\u5B9E\u73B0\u4E24\u4E2A\u53D8\u91CF\u7684\u4EA4\u6362"},{"level":2,"title":"\u786E\u4FDD\u5B57\u7B26\u4E32\u7684\u6BCF\u4E2A\u5355\u8BCD\u9996\u5B57\u6BCD\u90FD\u5927\u5199\uFF0C\u5176\u4F59\u90E8\u5206\u5C0F\u5199","slug":"\u786E\u4FDD\u5B57\u7B26\u4E32\u7684\u6BCF\u4E2A\u5355\u8BCD\u9996\u5B57\u6BCD\u90FD\u5927\u5199\uFF0C\u5176\u4F59\u90E8\u5206\u5C0F\u5199"},{"level":2,"title":"\u6E05\u9664\u5B57\u7B26\u4E32\u524D\u540E\u7684\u7A7A\u683C\uFF08\u517C\u5BB9\u6240\u6709\u6D4F\u89C8\u5668\uFF09","slug":"\u6E05\u9664\u5B57\u7B26\u4E32\u524D\u540E\u7684\u7A7A\u683C\uFF08\u517C\u5BB9\u6240\u6709\u6D4F\u89C8\u5668\uFF09"},{"level":2,"title":"\u53BB\u6389\u4E00\u7EC4\u6574\u578B\u6570\u7EC4\u4E2D\u91CD\u590D\u7684\u503C","slug":"\u53BB\u6389\u4E00\u7EC4\u6574\u578B\u6570\u7EC4\u4E2D\u91CD\u590D\u7684\u503C"},{"level":2,"title":"\u7FFB\u8F6C\u5B57\u7B26\u4E32","slug":"\u7FFB\u8F6C\u5B57\u7B26\u4E32"},{"level":2,"title":"\u627E\u5230\u63D0\u4F9B\u7684\u53E5\u5B50\u4E2D\u6700\u957F\u7684\u5355\u8BCD\uFF0C\u5E76\u8BA1\u7B97\u5B83\u7684\u957F\u5EA6\u3002","slug":"\u627E\u5230\u63D0\u4F9B\u7684\u53E5\u5B50\u4E2D\u6700\u957F\u7684\u5355\u8BCD\uFF0C\u5E76\u8BA1\u7B97\u5B83\u7684\u957F\u5EA6\u3002"},{"level":2,"title":"\u622A\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u5982\u679C\u5B57\u7B26\u4E32\u7684\u957F\u5EA6\u6BD4\u6307\u5B9A\u7684\u53C2\u6570num\u957F\uFF0C\u5219\u628A\u591A\u4F59\u7684\u90E8\u5206\u7528...\u6765\u8868\u793A","slug":"\u622A\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u5982\u679C\u5B57\u7B26\u4E32\u7684\u957F\u5EA6\u6BD4\u6307\u5B9A\u7684\u53C2\u6570num\u957F\uFF0C\u5219\u628A\u591A\u4F59\u7684\u90E8\u5206\u7528-\u6765\u8868\u793A"},{"level":2,"title":"\u5224\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\u4E2D\u51FA\u73B0\u6B21\u6570\u6700\u591A\u7684\u5B57\u7B26\uFF0C\u7EDF\u8BA1\u8FD9\u4E2A\u6B21\u6570","slug":"\u5224\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\u4E2D\u51FA\u73B0\u6B21\u6570\u6700\u591A\u7684\u5B57\u7B26\uFF0C\u7EDF\u8BA1\u8FD9\u4E2A\u6B21\u6570"},{"level":2,"title":"\u5FEB\u901F\u6392\u5E8F\uFF08Quick Sort\uFF09","slug":"\u5FEB\u901F\u6392\u5E8F\uFF08quick-sort\uFF09"},{"level":2,"title":"\u7528\u9012\u5F52\u5B9E\u73B0\u6590\u6CE2\u90A3\u5951\u6570\u5217","slug":"\u7528\u9012\u5F52\u5B9E\u73B0\u6590\u6CE2\u90A3\u5951\u6570\u5217"},{"level":2,"title":"\u6570\u7EC4\u4E2D\u4E24\u4E24\u4E4B\u5DEE\u7684\u6700\u5927\u503C\uFF1F","slug":"\u6570\u7EC4\u4E2D\u4E24\u4E24\u4E4B\u5DEE\u7684\u6700\u5927\u503C\uFF1F"},{"level":2,"title":"\u5355\u5411\u94FE\u8868\u53CD\u8F6C-JS\u5B9E\u73B0","slug":"\u5355\u5411\u94FE\u8868\u53CD\u8F6C-js\u5B9E\u73B0"},{"level":2,"title":"\u53D8\u5F62\u7684\u94FE\u8868\u53CD\u8F6C","slug":"\u53D8\u5F62\u7684\u94FE\u8868\u53CD\u8F6C"},{"level":2,"title":"\u5C0F\u7ED3","slug":"\u5C0F\u7ED3"}],"relativePath":"job/interview/4.md","lastUpdated":1645373384261}',a={},i=t(`<h1 id="\u7B97\u6CD5" tabindex="-1">\u7B97\u6CD5 <a class="header-anchor" href="#\u7B97\u6CD5" aria-hidden="true">#</a></h1><p>\u968F\u7740\u4E92\u8054\u7F51\u53D1\u5C55\u901F\u5EA6\u653E\u7F13\uFF0C\u4F01\u4E1A\u62DB\u8058\u65F6\u66F4\u6311\u4EBA\uFF0C\u6240\u4EE5\u8D8A\u6765\u8D8A\u591A\u7684\u516C\u53F8\uFF0C\u559C\u6B22\u5728\u9762\u8BD5\u4E2D\u8003\u5BDF\u7B97\u6CD5\uFF0C\u7279\u522B\u662F\u5728\u4E8C\u9762\u7684\u65F6\u5019\uFF0C\u6240\u4EE5\u7B97\u6CD5\u4E00\u5B9A\u5F97\u6709\u6240\u51C6\u5907\u3002</p><h2 id="\u5B9E\u73B0bind" tabindex="-1">\u5B9E\u73B0bind <a class="header-anchor" href="#\u5B9E\u73B0bind" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function deepClone\uFF08obj\uFF09{
+  if (obj === null | typeof obj !== &#39;object&#39;) return obj;
+  if (obj instanceof Date) return new Date(obj);
+  if (obj instanceof RegExp) return new Date(RegExp);
+  const newObj = new obj.constuctor;
+  for (let key in obj) {
+    if(obj.hasOwnProperty(key)) {
+      newObj[key] = deepClone(obj[key]);
+    }
+  }
+
+  return newObj;
+}
+</code></pre></div><h2 id="\u5B9E\u73B0\u6DF1\u62F7\u8D1D" tabindex="-1">\u5B9E\u73B0\u6DF1\u62F7\u8D1D <a class="header-anchor" href="#\u5B9E\u73B0\u6DF1\u62F7\u8D1D" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function deepClone\uFF08obj\uFF09{
+  if (obj === null | typeof obj !== &#39;object&#39;) return obj;
+  if (obj instanceof Date) return new Date(obj);
+  if (obj instanceof RegExp) return new Date(RegExp);
+  const newObj = new obj.constuctor;
+  for (let key in obj) {
+    if(obj.hasOwnProperty(key)) {
+      newObj[key] = deepClone(obj[key]);
+    }
+  }
+
+  return newObj;
+}
+</code></pre></div><h2 id="\u4E0D\u9700\u8981\u501F\u52A9\u7B2C\u4E09\u4E2A\u4E34\u65F6\u53D8\u91CF\uFF0C\u5B9E\u73B0\u4E24\u4E2A\u53D8\u91CF\u7684\u4EA4\u6362" tabindex="-1">\u4E0D\u9700\u8981\u501F\u52A9\u7B2C\u4E09\u4E2A\u4E34\u65F6\u53D8\u91CF\uFF0C\u5B9E\u73B0\u4E24\u4E2A\u53D8\u91CF\u7684\u4EA4\u6362 <a class="header-anchor" href="#\u4E0D\u9700\u8981\u501F\u52A9\u7B2C\u4E09\u4E2A\u4E34\u65F6\u53D8\u91CF\uFF0C\u5B9E\u73B0\u4E24\u4E2A\u53D8\u91CF\u7684\u4EA4\u6362" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function swap\uFF08a,b\uFF09{
+  b = b - a;
+  a = a + b;
+  b = a - b;
+  return [a,b];
+}
+</code></pre></div><h2 id="\u786E\u4FDD\u5B57\u7B26\u4E32\u7684\u6BCF\u4E2A\u5355\u8BCD\u9996\u5B57\u6BCD\u90FD\u5927\u5199\uFF0C\u5176\u4F59\u90E8\u5206\u5C0F\u5199" tabindex="-1">\u786E\u4FDD\u5B57\u7B26\u4E32\u7684\u6BCF\u4E2A\u5355\u8BCD\u9996\u5B57\u6BCD\u90FD\u5927\u5199\uFF0C\u5176\u4F59\u90E8\u5206\u5C0F\u5199 <a class="header-anchor" href="#\u786E\u4FDD\u5B57\u7B26\u4E32\u7684\u6BCF\u4E2A\u5355\u8BCD\u9996\u5B57\u6BCD\u90FD\u5927\u5199\uFF0C\u5176\u4F59\u90E8\u5206\u5C0F\u5199" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function titleCase(str) {
+  var lstr = str.toLowerCase().split(&#39; &#39;);
+  for(var i = 0 ; i &lt; lstr.length; i++) {
+    lstr[i] = lstr[i][0].toUpperCase() + lstr[i].substring(1, lstr[i].length);
+  }
+  var res = lstr.join(&#39; &#39;);
+  return res;
+}
+titleCase(&quot;good night&quot;); // Good Night
+</code></pre></div><h2 id="\u6E05\u9664\u5B57\u7B26\u4E32\u524D\u540E\u7684\u7A7A\u683C\uFF08\u517C\u5BB9\u6240\u6709\u6D4F\u89C8\u5668\uFF09" tabindex="-1">\u6E05\u9664\u5B57\u7B26\u4E32\u524D\u540E\u7684\u7A7A\u683C\uFF08\u517C\u5BB9\u6240\u6709\u6D4F\u89C8\u5668\uFF09 <a class="header-anchor" href="#\u6E05\u9664\u5B57\u7B26\u4E32\u524D\u540E\u7684\u7A7A\u683C\uFF08\u517C\u5BB9\u6240\u6709\u6D4F\u89C8\u5668\uFF09" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function trim(str) {
+  if (str &amp; typeof str === &quot;string&quot;) {
+    return str.replace(/(^s*)|(s*)$/g, &#39;&#39;); //\u53BB\u9664\u524D\u540E\u7A7A\u767D\u7B26
+  }
+}
+</code></pre></div><h2 id="\u53BB\u6389\u4E00\u7EC4\u6574\u578B\u6570\u7EC4\u4E2D\u91CD\u590D\u7684\u503C" tabindex="-1">\u53BB\u6389\u4E00\u7EC4\u6574\u578B\u6570\u7EC4\u4E2D\u91CD\u590D\u7684\u503C <a class="header-anchor" href="#\u53BB\u6389\u4E00\u7EC4\u6574\u578B\u6570\u7EC4\u4E2D\u91CD\u590D\u7684\u503C" aria-hidden="true">#</a></h2><div class="language-"><pre><code>let unique =  function(arr){
+  let hash={};
+  let data=[];
+  for (let i=0;i &lt; arr.length; i++){
+    if (!hash[arr[i]])  {
+      hash[arr[i]] = true;
+      data.push(arr[i]);
+    }      
+  }
+  return data
+}
+</code></pre></div><h2 id="\u7FFB\u8F6C\u5B57\u7B26\u4E32" tabindex="-1">\u7FFB\u8F6C\u5B57\u7B26\u4E32 <a class="header-anchor" href="#\u7FFB\u8F6C\u5B57\u7B26\u4E32" aria-hidden="true">#</a></h2><ul><li>split\uFF08\uFF09\u5B57\u7B26\u4E32\u8F6C\u6210\u6570\u7EC4\uFF1B</li><li>reverse()\u7FFB\u8F6C\u6570\u7EC4\uFF1B</li><li>join()\u6570\u7EC4\u8F6C\u5316\u6210\u5B57\u7B26\u4E32\u3002</li></ul><div class="language-"><pre><code>function reverseString(str){\xA0\xA0\xA0\xA0
+  return str.split(&#39;&#39;).reverse().join(&#39;&#39;);	
+}
+</code></pre></div><h2 id="\u627E\u5230\u63D0\u4F9B\u7684\u53E5\u5B50\u4E2D\u6700\u957F\u7684\u5355\u8BCD\uFF0C\u5E76\u8BA1\u7B97\u5B83\u7684\u957F\u5EA6\u3002" tabindex="-1">\u627E\u5230\u63D0\u4F9B\u7684\u53E5\u5B50\u4E2D\u6700\u957F\u7684\u5355\u8BCD\uFF0C\u5E76\u8BA1\u7B97\u5B83\u7684\u957F\u5EA6\u3002 <a class="header-anchor" href="#\u627E\u5230\u63D0\u4F9B\u7684\u53E5\u5B50\u4E2D\u6700\u957F\u7684\u5355\u8BCD\uFF0C\u5E76\u8BA1\u7B97\u5B83\u7684\u957F\u5EA6\u3002" aria-hidden="true">#</a></h2><ul><li>\u8F6C\u5316\u6210\u6570\u7EC4\uFF1B</li><li>\u6839\u636E\u5143\u7D20\u957F\u5EA6\u6392\u5E8F\uFF1B</li><li>\u8F93\u51FA\u6700\u957F\u5143\u7D20\u5E76\u8FD4\u56DE\u957F\u5EA6\u3002</li></ul><div class="language-"><pre><code>function findLongestString(str){
+\xA0\xA0var arr = str.split(&#39; &#39;);	
+\xA0\xA0var arrSort = arr.sort(function (a,b) {	\xA0\xA0\xA0
+\xA0\xA0\xA0\xA0return b.length - a.length;
+\xA0\xA0});
+  return [arrSort[0], arrSort[0].length];
+}
+</code></pre></div><h2 id="\u622A\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u5982\u679C\u5B57\u7B26\u4E32\u7684\u957F\u5EA6\u6BD4\u6307\u5B9A\u7684\u53C2\u6570num\u957F\uFF0C\u5219\u628A\u591A\u4F59\u7684\u90E8\u5206\u7528-\u6765\u8868\u793A" tabindex="-1">\u622A\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u5982\u679C\u5B57\u7B26\u4E32\u7684\u957F\u5EA6\u6BD4\u6307\u5B9A\u7684\u53C2\u6570num\u957F\uFF0C\u5219\u628A\u591A\u4F59\u7684\u90E8\u5206\u7528...\u6765\u8868\u793A <a class="header-anchor" href="#\u622A\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\uFF0C\u5982\u679C\u5B57\u7B26\u4E32\u7684\u957F\u5EA6\u6BD4\u6307\u5B9A\u7684\u53C2\u6570num\u957F\uFF0C\u5219\u628A\u591A\u4F59\u7684\u90E8\u5206\u7528-\u6765\u8868\u793A" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function truncate(str, num){
+  var trStr = str.slice(0, num);
+  if (trStr.length &gt; num) {					
+    return trStr.concat(&#39;...&#39;);
+  } else {
+    return str;
+  }
+}
+</code></pre></div><h2 id="\u5224\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\u4E2D\u51FA\u73B0\u6B21\u6570\u6700\u591A\u7684\u5B57\u7B26\uFF0C\u7EDF\u8BA1\u8FD9\u4E2A\u6B21\u6570" tabindex="-1">\u5224\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\u4E2D\u51FA\u73B0\u6B21\u6570\u6700\u591A\u7684\u5B57\u7B26\uFF0C\u7EDF\u8BA1\u8FD9\u4E2A\u6B21\u6570 <a class="header-anchor" href="#\u5224\u65AD\u4E00\u4E2A\u5B57\u7B26\u4E32\u4E2D\u51FA\u73B0\u6B21\u6570\u6700\u591A\u7684\u5B57\u7B26\uFF0C\u7EDF\u8BA1\u8FD9\u4E2A\u6B21\u6570" aria-hidden="true">#</a></h2><div class="language-"><pre><code>funcion findMaxStrCount(str) {
+  var countObj = {};
+  var max = &#39;&#39;;
+  for(var i = 0; i &lt; str.length; i++) {
+    var cur = str[i];
+    if(!countObj[cur]) {
+      countObj[cur] = 0;
+    } 
+    countObj[cur]++;
+    if(max === &#39;&#39; || countObj[cur] &gt; countObj[max]) { max = cur; }
+  }
+  return [max, countObj[max]];
+}
+
+</code></pre></div><h2 id="\u5FEB\u901F\u6392\u5E8F\uFF08quick-sort\uFF09" tabindex="-1">\u5FEB\u901F\u6392\u5E8F\uFF08Quick Sort\uFF09 <a class="header-anchor" href="#\u5FEB\u901F\u6392\u5E8F\uFF08quick-sort\uFF09" aria-hidden="true">#</a></h2><p>\u5FEB\u901F\u6392\u5E8F\u4F7F\u7528\u5206\u6CBB\u6CD5\u6765\u628A\u4E00\u4E2A\u4E32\uFF08list\uFF09\u5206\u4E3A\u4E24\u4E2A\u5B50\u4E32\uFF08sub-lists\uFF09\u3002\u5177\u4F53\u7B97\u6CD5\u63CF\u8FF0\u5982\u4E0B\uFF1A</p><div class="language-"><pre><code>var quickSort = function(arr) {
+\u3000if (arr.length &lt;= 1) { return arr; }
+\u3000var pivotIndex = Math.floor(arr.length / 2);
+\u3000var pivot = arr.splice(pivotIndex, 1)[0];
+\u3000var left = [];
+\u3000var right = [];
+\u3000for (var i = 0; i &lt; arr.length; i++){
+\u3000\u3000if (arr[i] &lt; pivot) {
+\u3000\u3000\u3000left.push(arr[i]);
+\u3000\u3000} else {
+\u3000\u3000\u3000right.push(arr[i]);
+\u3000\u3000}
+\u3000}
+\u3000return quickSort(left).concat([pivot], quickSort(right));
+};
+</code></pre></div><h2 id="\u7528\u9012\u5F52\u5B9E\u73B0\u6590\u6CE2\u90A3\u5951\u6570\u5217" tabindex="-1">\u7528\u9012\u5F52\u5B9E\u73B0\u6590\u6CE2\u90A3\u5951\u6570\u5217 <a class="header-anchor" href="#\u7528\u9012\u5F52\u5B9E\u73B0\u6590\u6CE2\u90A3\u5951\u6570\u5217" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function fib(n){
+  if(typeof n === &#39;number&#39; &amp;&amp; n &gt; 0 &amp;&amp; parseInt(n) === n ) {
+    return n &lt; 3 ? 1 : fib(n-1)+fib(n-2);\u3000
+  } else {
+    return &#39;\u8F93\u5165\u4E0D\u5408\u6CD5\uFF0C\u8BF7\u8F93\u5165\u6B63\u6574\u6570\uFF01&#39;\uFF1B
+  }\u3000\u3000\u3000
+}
+</code></pre></div><h2 id="\u6570\u7EC4\u4E2D\u4E24\u4E24\u4E4B\u5DEE\u7684\u6700\u5927\u503C\uFF1F" tabindex="-1">\u6570\u7EC4\u4E2D\u4E24\u4E24\u4E4B\u5DEE\u7684\u6700\u5927\u503C\uFF1F <a class="header-anchor" href="#\u6570\u7EC4\u4E2D\u4E24\u4E24\u4E4B\u5DEE\u7684\u6700\u5927\u503C\uFF1F" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function maxDiffInArray(arr){
+  if ( arr instanceof Array) {
+    if (arr.length) {
+      let max = arr[0], min = arr[0];
+      for (let i = 0; i &lt; arr.length; i++) {
+        if ( typeof arr[i]  !== &#39;number&#39; ) {
+          return &#39;\u8F93\u5165\u4E0D\u5408\u6CD5\uFF0C\u6570\u7EC4\u5B58\u5728\u975E\u6570\u5B57\u7C7B\u578B\u5143\u7D20\uFF0C\u8BF7\u8F93\u5165\u6570\u5B57\u6570\u7EC4&#39;;
+        }
+        max = Math.max(arr[i], max), min = Math.min(arr[i], min);
+      }   
+      return max - min;\u3000
+    } else {
+      return &#39;\u8F93\u5165\u4E0D\u5408\u6CD5\uFF0C\u6570\u7EC4\u4E3A\u7A7A\uFF0C\u8BF7\u8F93\u5165\u975E\u7A7A\u6570\u7EC4&#39;;
+    }
+  } else {
+    return &#39;\u8F93\u5165\u4E0D\u5408\u6CD5\uFF0C\u8BF7\u8F93\u5165\u975E\u7A7A\u6570\u7EC4&#39;;
+  }\u3000
+}
+</code></pre></div><h2 id="\u5355\u5411\u94FE\u8868\u53CD\u8F6C-js\u5B9E\u73B0" tabindex="-1">\u5355\u5411\u94FE\u8868\u53CD\u8F6C-JS\u5B9E\u73B0 <a class="header-anchor" href="#\u5355\u5411\u94FE\u8868\u53CD\u8F6C-js\u5B9E\u73B0" aria-hidden="true">#</a></h2><div class="language-"><pre><code>function reverseLinkedList(head) {
+     if (head === null || head.next === null) {
+         return head;
+     }
+     let newHead = null; 
+     let nextHead = null;
+     while (head) {
+        nextHead = head.next; // \u5148\u8BB0\u4F4F\u5F53\u524D\u8282\u70B9\u7684\u4E0B\u4E00\u8282\u70B9
+        head.next = newHead; // \u628A\u5F53\u524D\u8282\u70B9\u7684\u6307\u9488\u6307\u5411\u4E0A\u4E00\u8282\u70B9
+        newHead = head; // \u4FDD\u7559\u4E0A\u4E00\u8282\u70B9
+        head = nextHead; // \u904D\u5386\u4E0B\u4E00\u8282\u70B9
+     }
+     return newHead; // \u6700\u540Ehead \u4E3Anull\u8DF3\u51FA\u904D\u5386\uFF0C\u6B64\u65F6newHead\u5C31\u6210\u4E86\u65B0\u94FE\u8868\u7684\u5934\u7ED3\u70B9\u4E86
+}
+</code></pre></div><h2 id="\u53D8\u5F62\u7684\u94FE\u8868\u53CD\u8F6C" tabindex="-1">\u53D8\u5F62\u7684\u94FE\u8868\u53CD\u8F6C <a class="header-anchor" href="#\u53D8\u5F62\u7684\u94FE\u8868\u53CD\u8F6C" aria-hidden="true">#</a></h2><p>\u7ED9\u5B9A\u4E00\u4E2A\u5355\u94FE\u8868\u7684\u5934\u8282\u70B9 head,\u5B9E\u73B0\u4E00\u4E2A\u8C03\u6574\u5355\u94FE\u8868\u7684\u51FD\u6570\uFF0C\u4F7F\u5F97\u6BCFK\u4E2A\u8282\u70B9\u4E4B\u95F4\u4E3A\u4E00\u7EC4\u8FDB\u884C\u9006\u5E8F\uFF0C\u5E76\u4E14\u4ECE\u94FE\u8868\u7684\u5C3E\u90E8\u5F00\u59CB\u7EC4\u8D77\uFF0C\u5934\u90E8\u5269\u4F59\u8282\u70B9\u6570\u91CF\u4E0D\u591F\u4E00\u7EC4\u7684\u4E0D\u9700\u8981\u9006\u5E8F\u3002</p><p>\u4F8B\u5982\uFF1A\u94FE\u8868:1-&gt;2-&gt;3-&gt;4-&gt;5-&gt;6-&gt;7-&gt;8-&gt;null, K = 3\u3002\u90A3\u4E48 6-&gt;7-&gt;8\uFF0C3-&gt;4-&gt;5\uFF0C1-&gt;2\u5404\u4F4D\u4E00\u7EC4\u3002\u8C03\u6574\u540E\uFF1A1-&gt;2-&gt;5-&gt;4-&gt;3-&gt;8-&gt;7-&gt;6-&gt;null\u3002\u5176\u4E2D 1\uFF0C2\u4E0D\u8C03\u6574\uFF0C\u56E0\u4E3A\u4E0D\u591F\u4E00\u7EC4\u3002</p><div class="language-"><pre><code>    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp = head;
+        for (int i = 1; i &lt; k &amp;&amp; temp != null; i++) {
+            temp = temp.next;
+        }
+        //\u5224\u65AD\u8282\u70B9\u7684\u6570\u91CF\u662F\u5426\u80FD\u591F\u51D1\u6210\u4E00\u7EC4
+        if(temp == null)
+            return head;
+
+        ListNode t2 = temp.next;
+        temp.next = null;
+        //\u628A\u5F53\u524D\u7684\u7EC4\u8FDB\u884C\u9006\u5E8F
+        ListNode newHead = reverseList(head);
+        //\u628A\u4E4B\u540E\u7684\u8282\u70B9\u8FDB\u884C\u5206\u7EC4\u9006\u5E8F
+        ListNode newTemp = reverseKGroup(t2, k);
+        // \u628A\u4E24\u90E8\u5206\u8FDE\u63A5\u8D77\u6765
+        head.next = newTemp;
+        
+        return newHead;
+    }
+    
+    //\u9006\u5E8F\u5355\u94FE\u8868
+    private static ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode result = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return result;
+    }
+
+public ListNode solve(ListNode head, int k) {
+    // \u8C03\u7528\u9006\u5E8F\u51FD\u6570
+    head = reverse(head);
+    // \u8C03\u7528\u6BCF k \u4E2A\u4E3A\u4E00\u7EC4\u7684\u9006\u5E8F\u51FD\u6570\uFF08\u4ECE\u5934\u90E8\u5F00\u59CB\u7EC4\u8D77\uFF09
+    head = reverseKGroup(head, k);
+    // \u5728\u9006\u5E8F\u4E00\u6B21
+    head = reverse(head);
+    return head;
+}
+</code></pre></div><h2 id="\u5C0F\u7ED3" tabindex="-1">\u5C0F\u7ED3 <a class="header-anchor" href="#\u5C0F\u7ED3" aria-hidden="true">#</a></h2><p>\u8FD9\u91CC\u6240\u5217\u4E3E\u7684\u4E00\u4E9B\u7B97\u6CD5\u9898\uFF0C\u5E94\u4ED8\u591A\u6570\u4E92\u8054\u7F51\u516C\u53F8\u5E94\u8BE5\u5C31\u53EF\u4EE5\u4E86\uFF0C\u4E0D\u8FC7\u82E5\u60F3\u53BB\u5B57\u8282\u8DF3\u52A8\u8FD9\u4E2A\u7B97\u6CD5\u9A71\u52A8\u7684\u516C\u53F8\uFF0C\u6700\u540E\u8FD8\u662F\u7684\u53BBLeecode\u7B49\u5237\u9898\u7F51\u7AD9\u5237\u4E00\u767E\u9898\u4EE5\u4E0A\u7684\u7B97\u6CD5\u3002</p>`,39),l=[i];function d(s,o,u,h,c,p){return r(),n("div",null,l)}var v=e(a,[["render",d]]);export{f as __pageData,v as default};
