cc7b60b4d3ef70dab9d4de37cac1268399d6c0ba
kongwutw <chentianwen@yxqiche.com>
2022-02-21 02:22:34 +0800

    docs: add some doc

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -3,6 +3,8 @@
 /**
  * @type {import('vitepress').UserConfig}
  */
+
+ 
 module.exports = {
   "lang": "zh-cn",
   outDir: '../docs',
@@ -16,8 +18,9 @@ module.exports = {
     docsDir: 'docs',
     docsBranch: 'main',
     nav: [
-      { text: '技术文章', link: '/tech/' },
-      { text: '管理笔记', link: '/manage/' },
+      { text: '专业技能', link: '/tech/' },
+      { text: '通用能力', link: '/manage/' },
+      { text: '职场心得', link: '/job/interview/' },
       { text: '关于小编', link: '/about/' },
       {
         text: '常用网址',
@@ -59,6 +62,62 @@ module.exports = {
     ],
     sidebar: {
       '/about': 'auto',
+      '/job': [
+        {
+          text: '面试宝典',
+          children: [
+            {
+              text: '准备简历',
+              link: '/job/interview/'
+            },
+            {
+              text: 'web基础',
+              link: '/job/interview/2'
+            },
+            {
+              text: '深入JS',
+              link: '/job/interview/3'
+            },
+            {
+              text: '算法',
+              link: '/job/interview/4'
+            },
+            {
+              text: 'vue',
+              link: '/job/interview/5'
+            },
+            {
+              text: 'react',
+              link: '/job/interview/6'
+            },
+            {
+              text: '性能优化',
+              link: '/job/interview/7'
+            },
+            {
+              text: '浏览器',
+              link: '/job/interview/8'
+            },
+          ]
+        },
+        {
+          text: '高效工作',
+          children: [
+            {
+              text: '十条经验',
+              link: '/job/think/ten'
+            },
+            {
+              text: '如何高效？',
+              link: '/job/think/how'
+            },
+            {
+              text: '七个习惯',
+              link: '/job/think/seven'
+            },
+          ]
+        },
+      ],
       '/tech': [
         {
           text: '技术调研',
@@ -107,6 +166,96 @@ module.exports = {
             {
               text: 'React 和 Vue 中的 Hooks 对比',
               link: '/tech/di/hooks'
+            },
+          ]
+        },
+        {
+          text: '基础知识',
+          children: [
+            {
+              text: 'ES5 读书指南',
+              link: '/tech/es5q'
+            },
+            {
+              text: '红宝书 读书指南',
+              link: '/tech/hbs'
+            },
+          ]
+        },
+        {
+          text: 'JS全栈入门教程',
+          children: [
+            {
+              text: '1_1 教程简介',
+              link: '/tech/site/1_1'
+            },
+            {
+              text: '1_2 最终代码和效果',
+              link: '/tech/site/1_2'
+            },
+            {
+              text: '1_3 学习建议',
+              link: '/tech/site/1_3'
+            },
+            {
+              text: '2_1 node简介',
+              link: '/tech/site/2_1'
+            },
+            {
+              text: '2_2 vscode',
+              link: '/tech/site/2_2'
+            },
+            {
+              text: '3_1 Linux 入门',
+              link: '/tech/site/3_1'
+            },
+            {
+              text: '3_2 初始化项目',
+              link: '/tech/site/3_2'
+            },
+            {
+              text: '3_3 koa-static处理静态资源',
+              link: '/tech/site/3_3'
+            },
+            {
+              text: '3_4 分析 koa-static 的源码',
+              link: '/tech/site/3_4'
+            },
+            {
+              text: '4_1 koa 简介',
+              link: '/tech/site/4_1'
+            },
+            {
+              text: '4_2 koa 源码',
+              link: '/tech/site/4_2'
+            },
+            {
+              text: '4_3 koa 中间件',
+              link: '/tech/site/4_3'
+            },
+            {
+              text: '5_1 从0实现响应式导航栏',
+              link: '/tech/site/5_1'
+            },
+            {
+              text: '5_2 导航栏的吸顶',
+              link: '/tech/site/5_2'
+            },
+            {
+              text: '5_3 导航栏响应滚动',
+              link: '/tech/site/5_3'
+            },
+            {
+              text: '6_1 上线部署前的准备',
+              link: '/tech/site/6_1'
+            },
+            {
+              text: '6_2 基于node服务器部署静态网站',
+              link: '/tech/site/6_2'
+            },
+            {
+              text: '6_3 基于ngnix部署静态网站',
+              link: '/tech/site/6_3'
             },
           ]
         },
