8c688a6506fff81d351847fb89b2de241f7f77b4
pascal <kongwutianwen@163.com>
2022-02-25 09:24:38 +0000

    deploy

---
+++ docs/assets/tech_site_4_2.md.2c8e4198.js
@@ -0,0 +1,33 @@
+import{_ as e,c as a,o as n,a as t}from"./app.0c9aa2df.js";const f='{"title":"4_2 koa \u6E90\u7801","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u6E90\u7801\u7ED3\u6784","slug":"\u6E90\u7801\u7ED3\u6784"},{"level":2,"title":"\u4E00\u4E9B\u5E38\u7528\u65B9\u6CD5\u7684\u5B9E\u73B0","slug":"\u4E00\u4E9B\u5E38\u7528\u65B9\u6CD5\u7684\u5B9E\u73B0"}],"relativePath":"tech/site/4_2.md","lastUpdated":1645781043007}',s={},r=t(`<h1 id="_4-2-koa-\u6E90\u7801" tabindex="-1">4_2 koa \u6E90\u7801 <a class="header-anchor" href="#_4-2-koa-\u6E90\u7801" aria-hidden="true">#</a></h1><p>\u672C\u8282\u5C06\u5E26\u7740\u5927\u5BB6\u7B80\u5355\u770B\u4E00\u4E0Bkoa2.x\u6700\u65B0\u7248\u672C\u7684\u6E90\u7801\u3002</p><h2 id="\u6E90\u7801\u7ED3\u6784" tabindex="-1">\u6E90\u7801\u7ED3\u6784 <a class="header-anchor" href="#\u6E90\u7801\u7ED3\u6784" aria-hidden="true">#</a></h2><p>\u5728\u4E0A\u4E00\u7AE0\u6211\u4EEC\u5DF2\u7ECF\u5206\u4EAB\u4E86\u67E5\u770B\u4F9D\u8D56\u5E93\u6E90\u7801\u7684\u65B9\u6CD5\uFF0C\u672C\u8282\u6211\u4EEC\u8FD8\u662F\u57FA\u4E8E\u65B9\u5F0F\u4E00\uFF0C\u76F4\u63A5\u5230\u9879\u76EE\u4E0Bde node_modules\u76EE\u5F55\u4E0B\u627E\u5230koa\u3002\u5305\u542B\u4E86\u5982\u4E0B\u6587\u4EF6\uFF1A</p><div class="language-"><pre><code>\u251C\u2500\u2500 History.md // \u5386\u53F2\u7248\u672C\u7BA1\u7406
+\u251C\u2500\u2500 LICENSE // \u5F00\u6E90\u534F\u8BAE
+\u251C\u2500\u2500 Readme.md // \u4F9D\u8D56\u5E93\u7684\u6709\u5173\u4ECB\u7ECD
+\u251C\u2500\u2500 lib
+\u2502\xA0\xA0 \u251C\u2500\u2500 application.js //\u5165\u53E3\u6587\u4EF6\uFF0C\u5C01\u88C5\u4E86context\uFF0Crequest\uFF0Cresponse\uFF0C\u4EE5\u53CA\u6700\u6838\u5FC3\u7684\u4E2D\u95F4\u4EF6\u5904\u7406\u6D41\u7A0B\u3002
+\u2502\xA0\xA0 \u251C\u2500\u2500 context.js //\u5904\u7406\u5E94\u7528\u4E0A\u4E0B\u6587\uFF0C\u91CC\u9762\u76F4\u63A5\u5C01\u88C5\u90E8\u5206request.js\u548Cresponse.js\u7684\u65B9\u6CD5
+\u2502\xA0\xA0 \u251C\u2500\u2500 request.js // \u5904\u7406http\u8BF7\u6C42
+\u2502\xA0\xA0 \u2514\u2500\u2500 response.js // \u5904\u7406http\u54CD\u5E94
+\u2514\u2500\u2500 package.json // \u6A21\u5757\u7BA1\u7406
+</code></pre></div><p>\u6211\u4EEC\u770B\u5230\u6E90\u7801\u90FD\u5728lib\u76EE\u5F55\u4E0B, koa2.x\u4F5C\u4E3A\u4E00\u4E2Aweb\u6846\u67B6\uFF0C\u53EA\u63D0\u4F9B\u4E86\u5C01\u88C5\u597D\u7684HTTP\u670D\u52A1\uFF0C\u4EE5\u53CA\u57FA\u4E8Easync/await\u7684\u4E2D\u95F4\u4EF6\u5BB9\u5668\u3002\u7528Koa.js\u60F3\u5B9E\u73B0\u5927\u90E8\u5206Web\u529F\u80FD\u7684\u8BDD\uFF0C\u5C31\u9700\u8981\u901A\u8FC7\u4E2D\u95F4\u4EF6\u6765\u5B9E\u73B0\uFF0C\u5982\u6211\u4EEC\u524D\u9762\u7528\u5230\u7684koa-static\u3002</p><h2 id="\u4E00\u4E9B\u5E38\u7528\u65B9\u6CD5\u7684\u5B9E\u73B0" tabindex="-1">\u4E00\u4E9B\u5E38\u7528\u65B9\u6CD5\u7684\u5B9E\u73B0 <a class="header-anchor" href="#\u4E00\u4E9B\u5E38\u7528\u65B9\u6CD5\u7684\u5B9E\u73B0" aria-hidden="true">#</a></h2><ul><li>listen\u5982\u4F55\u5B9E\u73B0\u7684\uFF1F</li></ul><div class="language-"><pre><code> listen(...args) {
+    debug(&#39;listen&#39;);
+    const server = http.createServer(this.callback());
+    return server.listen(...args);
+  }
+</code></pre></div><ul><li>app.use() \u5E72\u4E86\u5565\uFF1F</li></ul><div class="language-"><pre><code>  use(fn) {
+    ...\u7701\u7565\u4E86\u5404\u79CD\u5F02\u5E38\u548C\u517C\u5BB9\u5904\u7406
+    if (isGeneratorFunction(fn)) {
+      fn = convert(fn); 
+      // koa@2\u4E2D\u95F4\u4EF6\u53EA\u652F\u6301 async/await \u5C01\u88C5\u7684\uFF0C\u5982\u679C\u8981\u4F7F\u7528koa@1\u57FA\u4E8Egenerator\u4E2D\u95F4\u4EF6\uFF0C\u9700\u8981\u901A\u8FC7\u4E2D\u95F4\u4EF6koa-convert\u5C01\u88C5\u4E00\u4E0B\u624D\u80FD\u4F7F\u7528
+    }
+    this.middleware.push(fn);
+    return this;
+  }
+</code></pre></div><ul><li>callback \u6E90\u7801</li></ul><div class="language-"><pre><code>  callback() {
+    const fn = compose(this.middleware);
+    if (!this.listenerCount(&#39;error&#39;)) this.on(&#39;error&#39;, this.onerror);
+    const handleRequest = (req, res) =&gt; {
+      const ctx = this.createContext(req, res);
+      return this.handleRequest(ctx, fn);
+    };
+    return handleRequest;
+  }
+</code></pre></div><p>\u53EF\u4EE5\u770B\u5230\uFF0C\u5728\u6211\u4EEC\u5B9E\u4F8B\u5316 <code>const app = new koa();</code> \u540E\uFF0C\u6267\u884C <code>app.use()</code> \u7CFB\u5217\u4E2D\u95F4\u4EF6\u540E\uFF0C\u4F1A\u628A\u5404\u4E2D\u95F4\u4EF6\u6DFB\u52A0\u8FDB middleware \u8FD9\u4E2A\u6570\u7EC4\uFF0C\u7136\u540E\u518D\u6267\u884C<code>app.listen()</code> \u7684\u65F6\u5019\uFF0C\u4F1A\u628Acallback()\u56DE\u8C03\u51FD\u6570\u4F20\u5165 node\u539F\u751F\u7684http\u6A21\u5757\u7684createServer()\u65B9\u6CD5\uFF0C\u7136\u540E\u5728\u542F\u52A8\u670D\u52A1\u5668\u4EE5\u540E\u5C31\u4F1A\u6267\u884Ccallback()\u4E2D\u8BF7\u6C42\u3001\u54CD\u5E94\u3001\u4E0A\u4E0B\u6587\u4EE5\u53CA\u4E2D\u95F4\u4EF6\u7684\u6709\u5173\u903B\u8F91\u4E86\u3002\u66F4\u6DF1\u5165\u7684\u63A2\u7A76\uFF0C\u5C31\u662F\u57FA\u4E8E\u6B64\u4E00\u6B65\u6B65\u5206\u6790\u5404\u4E2A\u6A21\u5757\u7684\u5B9E\u73B0\u4E86\u3002</p>`,14),o=[r];function c(i,d,l,p,h,u){return n(),a("div",null,o)}var k=e(s,[["render",c]]);export{f as __pageData,k as default};
---
+++ docs/assets/tech_site_5_1.md.ccc2dd69.js
@@ -0,0 +1,172 @@
+import{_ as t,c as n,o as a,a as e}from"./app.0c9aa2df.js";const f='{"title":"5_1 \u4ECE0\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F","slug":"\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F"},{"level":2,"title":"\u5E03\u5C40\u7684\u5E38\u8BC6","slug":"\u5E03\u5C40\u7684\u5E38\u8BC6"},{"level":2,"title":"\u5982\u4F55\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F","slug":"\u5982\u4F55\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F"},{"level":2,"title":"\u6700\u7EC8\u6548\u679C","slug":"\u6700\u7EC8\u6548\u679C"}],"relativePath":"tech/site/5_1.md","lastUpdated":1645781043007}',l={},o=e(`<h1 id="_5-1-\u4ECE0\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" tabindex="-1">5_1 \u4ECE0\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F <a class="header-anchor" href="#_5-1-\u4ECE0\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" aria-hidden="true">#</a></h1><p>\u672C\u8282\u5C06\u5206\u4EAB\u5982\u4F55\u4ECE0\u5B9E\u73B0\u4E00\u4E2A\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F\u3002</p><p>\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F\u662F\u9759\u6001\u7F51\u7AD9\u7279\u522B\u5E38\u89C1\u7684\u9700\u6C42\uFF0C\u591A\u6570\u7F51\u7AD9\u90FD\u662F\u57FA\u4E8ETwitter\u63A8\u51FA\u7684Bootstrap\u3002\u6240\u4EE5\u638C\u63E1\u4ECE0\u5F00\u53D1\u4E00\u4E2A\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F\u7684\u524D\u7AEF\u5F88\u5C11\uFF0C\u672C\u8282\u5C06\u5E26\u5927\u5BB6\u4E00\u63A2\u7A76\u7ADF\u3002</p><h2 id="\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" tabindex="-1">\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F <a class="header-anchor" href="#\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" aria-hidden="true">#</a></h2><p>\u8981\u60F3 \u77E5\u9053\u4EC0\u4E48\u662F\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F\uFF0C\u6211\u4EEC\u9700\u8981\u4ECE\u4E24\u4E2A\u65B9\u9762\u53BB\u7406\u89E3\uFF1A</p><ul><li>\u9996\u5148\uFF0C\u662F\u5BFC\u822A\u680F\u7684\u5B9A\u4E49\uFF1A\u6307\u4F4D\u4E8E\u9875\u9762\u9876\u90E8\u6216\u8005\u4FA7\u8FB9\u533A\u57DF\u7684\u4E00\u6392\u6C34\u5E73\u5BFC\u822A\u6309\u94AE\uFF0C\u8D77\u7740\u94FE\u63A5\u7AD9\u70B9\u6216\u8005\u8F6F\u4EF6\u5185\u7684\u5404\u4E2A\u9875\u9762\u7684\u4F5C\u7528\uFF1B</li><li>\u5176\u6B21\uFF0C\u662F\u54CD\u5E94\u5F0F\u7684\u5B9A\u4E49\uFF1A\u54CD\u5E94\u5F0F\u786E\u5207\u79F0\u4E3A\u54CD\u5E94\u5F0F\u5E03\u5C40\uFF0C\u662FEthan Marcotte\u57282010\u5E745\u6708\u4EFD\u63D0\u51FA\u7684\u4E00\u4E2A\u6982\u5FF5\uFF0C\u7B80\u800C\u8A00\u4E4B\uFF0C\u5C31\u662F\u4E00\u4E2A\u7F51\u7AD9\u80FD\u591F\u517C\u5BB9\u591A\u4E2A\u7EC8\u7AEF\u2014\u2014\u800C\u4E0D\u662F\u4E3A\u6BCF\u4E2A\u7EC8\u7AEF\u505A\u4E00\u4E2A\u7279\u5B9A\u7684\u7248\u672C\uFF0C\u5B83\u662F\u4E3A\u89E3\u51B3\u79FB\u52A8\u4E92\u8054\u7F51\u6D4F\u89C8\u800C\u8BDE\u751F\u7684\u3002</li></ul><p>\u7ED3\u5408\u54CD\u5E94\u5F0F\u5E03\u5C40\u548C\u5BFC\u822A\u680F\u7684\u6982\u5FF5\uFF0C\u6211\u4EEC\u5C31\u77E5\u9053\u6211\u4EEC\u8981\u5B9E\u73B0\u7684\u662F\u4E00\u7EC4\u517C\u5BB9\u4E0D\u540C\u7EC8\u7AEF\u7684\u5BFC\u822A\u6309\u94AE\u3002</p><h2 id="\u5E03\u5C40\u7684\u5E38\u8BC6" tabindex="-1">\u5E03\u5C40\u7684\u5E38\u8BC6 <a class="header-anchor" href="#\u5E03\u5C40\u7684\u5E38\u8BC6" aria-hidden="true">#</a></h2><p>\u7F51\u9875\u5236\u4F5C\u6700\u91CD\u8981\u7684\u5C31\u662F\u7F51\u9875\u5E03\u5C40\uFF0C\u5148\u5E03\u5C40\uFF0C\u540E\u7EC6\u8282\uFF0C\u53EA\u6709\u5728\u9009\u62E9\u4E86\u5408\u9002\u7684\u7F51\u7AD9\u5E03\u5C40\u4EE5\u540E\uFF0C\u624D\u53EF\u4EE5\u987A\u5229\u7684\u8FDB\u884C\u63A5\u4E0B\u6765\u7684\u5DE5\u4F5C\u3002</p><p>\u597D\u7684\u7F51\u9875\u5E03\u5C40\u80FD\u591F\u8BA9\u524D\u7AEF\u5F00\u53D1\u4EBA\u5458\u66F4\u597D\u5730\u628A\u63E1\u7F51\u9875\u7684\u6574\u4F53\u7ED3\u6784\uFF0C\u63D0\u9AD8\u4EE3\u7801\u7684\u4E66\u5199\u6548\u7387\uFF0C\u590D\u7528\u6027\uFF0C\u548C\u540E\u671F\u7EF4\u62A4\u3002</p><p>\u5E38\u89C1\u7684\u5E03\u5C40\u6709\uFF1A</p><ul><li>\u9759\u6001\u5E03\u5C40\uFF1A\u5373\u6700\u5F00\u59CB\u7684\u7F51\u9875\u8BBE\u8BA1\uFF0C\u6240\u6709\u7684\u7F51\u9875\u5C3A\u5BF8\u90FD\u7528px\u4F5C\u4E3A\u5355\u4F4D\uFF1B</li><li>\u6D41\u5F0F\u5E03\u5C40\uFF1A\u4F7F\u7528%\u767E\u5206\u6BD4\u5B9A\u4E49\u5BBD\u5EA6\uFF0C\u9875\u9762\u5143\u7D20\u5BBD\u5EA6\u6309\u7167\u5C4F\u5E55\u5206\u8FA8\u7387\u8FDB\u884C\u9002\u914D\u8C03\u6574\uFF0C\u5143\u7D20\u5927\u5C0F\u4F1A\u53D8\u5316\u4F46\u6574\u4F53\u5E03\u5C40\u4E0D\u4FBF\uFF0C\u9700\u8981\u914D\u5408max-width\u6216min-width\u7B49\u5C5E\u6027\u63A7\u5236\u5C3A\u5BF8\u6D41\u52A8\u8303\u56F4\uFF0C\u5982\u6805\u683C\u7CFB\u7EDF\uFF1B</li><li>\u81EA\u9002\u5E94\u5E03\u5C40\uFF1A\u5206\u522B\u4E3A\u4E0D\u540C\u7684\u5C4F\u5E55\u5206\u8FA8\u7387\u5B9A\u4E49\u5E03\u5C40\uFF0C\u5373\u521B\u5EFA\u591A\u4E2A\u9759\u6001\u5E03\u5C40\uFF0C\u6BCF\u4E2A\u9759\u6001\u5E03\u5C40\u5BF9\u5E94\u4E00\u4E2A\u8303\u56F4\uFF1B</li><li>\u54CD\u5E94\u5F0F\u5E03\u5C40\uFF1A\u7ED3\u5408\u6D41\u6C34\u5E03\u5C40\u548C\u81EA\u9002\u5E94\u5E03\u5C40\uFF0C\u642D\u914D\u5A92\u4F53\u67E5\u8BE2\u6280\u672F\u7684\u4F7F\u7528\uFF0C\u5143\u7D20\u4F4D\u7F6E\u548C\u5927\u5C0F\u90FD\u4F1A\u53D8\uFF0C\u9002\u5E94pc\u548C\u79FB\u52A8\u7AEF\uFF0C\u548C\u81EA\u9002\u5E94\u4E00\u6837\uFF0C\u90FD\u9700\u8981\u68C0\u6D4B\u8BBE\u5907\uFF0C\u6839\u636E\u4E0D\u540C\u8BBE\u5907\u5C55\u793A\u4E0D\u540C\u7684\u6837\u5F0F\uFF1B</li><li>\u5F39\u6027\u5E03\u5C40\uFF1A\u5305\u88F9\u6587\u5B57\u5404\u5143\u7D20\u7684\u5C3A\u5BF8\u91C7\u7528em/rem(\u4E3A\u987A\u5E94\u4E0D\u540C\u7F51\u9875\u5B57\u4F53\u5927\u5C0F\u5C55\u73B0\u800C\u4EA7\u751F\u7684\uFF0Cem\u76F8\u5BF9\u5176\u7236\u5143\u7D20\uFF0Crem\u59CB\u7EC8\u76F8\u5BF9\u9875\u9762\u6839\u5143\u7D20)\u505A\u5355\u4F4D\uFF0C\u800C\u9875\u9762\u4E3B\u8981\u5212\u5206\u533A\u57DF\u7684\u5C3A\u5BF8\u4ECD\u4F7F\u7528\u767E\u5206\u6570\u6216px\uFF0C\u6D4F\u89C8\u5668\u9ED8\u8BA4\u5B57\u4F5316px\uFF0C\u6240\u4EE51 em = 16px\uFF0C\u4E3A\u4E86\u8BA1\u7B97\u65B9\u4FBF\u901A\u5E38\u5C06\u9875\u9762\u6839\u5143\u7D20\u8BBE\u7F6E\u4E3A62.5%\uFF0C1 rem = 10px\u3002</li></ul><p>\u5E03\u5C40\u9009\u62E9\u5EFA\u8BAE\uFF1A</p><ul><li>\u5982\u679C\u53EA\u505APC\u7AEF\uFF0C\u9759\u6001\u5E03\u5C40\u662F\u6700\u597D\u7684\u9009\u62E9\uFF1B</li><li>\u5982\u679C\u53EA\u505A\u79FB\u52A8\u7AEF\uFF0C\u4E14\u9AD8\u5EA6\u548C\u5143\u7D20\u95F4\u8DDD\u4E0D\u9AD8\uFF0C\u5F39\u6027\u5E03\u5C40\u662F\u6700\u597D\u7684\u9009\u62E9\uFF1B</li><li>\u5982\u679C\u517C\u5BB9PC\u548C\u79FB\u52A8\u7AEF\uFF0C\u54CD\u5E94\u5F0F\u5E03\u5C40\u662F\u6700\u597D\u7684\u9009\u62E9\u3002</li></ul><p>\u663E\u7136\uFF0C\u6211\u4EEC\u8981\u505A\u7684\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F\u5C31\u662F\u8981\u517C\u5BB9\u591A\u7AEF\uFF0C\u6240\u4EE5\u9700\u8981\u91C7\u7528\u54CD\u5E94\u5F0F\u5E03\u5C40\u3002</p><h2 id="\u5982\u4F55\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" tabindex="-1">\u5982\u4F55\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F <a class="header-anchor" href="#\u5982\u4F55\u5B9E\u73B0\u54CD\u5E94\u5F0F\u5BFC\u822A\u680F" aria-hidden="true">#</a></h2><ul><li>\u8BBE\u7F6E Meta \u6807\u7B7E\uFF1A\u5927\u591A\u6570\u79FB\u52A8\u6D4F\u89C8\u5668\u5C06HTML\u9875\u9762\u653E\u5927\u4E3A\u5BBD\u7684\u89C6\u56FE\uFF08viewport\uFF09\u4EE5\u7B26\u5408\u5C4F\u5E55\u5206\u8FA8\u7387\u3002\u4F60\u53EF\u4EE5\u4F7F\u7528\u89C6\u56FE\u7684meta\u6807\u7B7E\u6765\u8FDB\u884C\u91CD\u7F6E\u3002\u4E0B\u9762\u7684\u89C6\u56FE\u6807\u7B7E\u544A\u8BC9\u6D4F\u89C8\u5668\uFF0C\u4F7F\u7528\u8BBE\u5907\u7684\u5BBD\u5EA6\u4F5C\u4E3A\u89C6\u56FE\u5BBD\u5EA6\u5E76\u7981\u6B62\u521D\u59CB\u7684\u7F29\u653E\u3002\u5728<code>&lt;head&gt;</code>\u6807\u7B7E\u91CC\u52A0\u5165\u8FD9\u4E2Ameta\u6807\u7B7E\u3002</li></ul><div class="language-"><pre><code>&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no&quot;&gt;
+</code></pre></div><p>user-scalable = no \u5C5E\u6027\u80FD\u591F\u89E3\u51B3 iPad \u5207\u6362\u6A2A\u5C4F\u4E4B\u540E\u89E6\u6478\u624D\u80FD\u56DE\u5230\u5177\u4F53\u5C3A\u5BF8\u7684\u95EE\u9898\u3002</p><ul><li>\u9996\u5148\uFF0C\u8BBE\u5B9A\u9875\u9762\u7684\u5927\u4F53\u5E03\u5C40\uFF0C\u5728index.html\u6587\u4EF6\u4E2D\u52A0\u5165\u5982\u4E0B\u5185\u5BB9\uFF1A</li></ul><div class="language-"><pre><code>    &lt;header&gt;
+        &lt;nav&gt;
+            &lt;div class=&quot;navbar-header&quot;&gt;
+              &lt;a href=&quot;#&quot; class=&quot;navbar-logo&quot;&gt;Logo&lt;/a&gt;
+              &lt;button class=&quot;navbar-toggle&quot; type=&quot;button&quot;&gt;
+                &lt;span class=&quot;icon-bar&quot;&gt;&lt;/span&gt;
+                &lt;span class=&quot;icon-bar&quot;&gt;&lt;/span&gt;
+                &lt;span class=&quot;icon-bar&quot;&gt;&lt;/span&gt;
+              &lt;/button&gt;
+            &lt;/div&gt;
+            &lt;div class=&quot;collapse navbar-collapse&quot;&gt;
+              &lt;ul class=&quot;nav navbar-nav&quot; id=&quot;navList&quot;&gt;
+                &lt;li&gt;&lt;a href=&quot;#&quot; class=&quot;active&quot;&gt;\u9996\u9875&lt;/a&gt;&lt;/li&gt;
+                &lt;li&gt;&lt;a href=&quot;#&quot;&gt;\u4EA7\u54C1\u670D\u52A1&lt;/a&gt;&lt;/li&gt;
+                &lt;li&gt;&lt;a href=&quot;#&quot;&gt;\u8054\u7CFB\u6211\u4EEC&lt;/a&gt;&lt;/li&gt;
+                &lt;li&gt;&lt;a href=&quot;#&quot;&gt;\u5173\u4E8E\u6211\u4EEC&lt;/a&gt;&lt;/li&gt;
+               &lt;/ul&gt;
+            &lt;/div&gt;
+          &lt;/nav&gt;
+    &lt;/header&gt;
+    &lt;section class=&quot;content-wrap&quot;&gt;
+      &lt;!-- \u7F51\u7AD9\u5185\u5BB9 --&gt;
+      &lt;img src=&quot;skills.jpg&quot; alt=&quot;\u524D\u7AEF\u6280\u80FD\u56FE&quot;&gt;
+    &lt;/section&gt;
+    &lt;footer class=&quot;foot-wrap&quot;&gt;
+        &lt;p&gt;\xA92018 \u6155\u8BFE\u7F51\u5B66\u4E60 &lt;/p&gt;
+        &lt;p&gt;\u4EACICP\u5907180043275\u53F7-2&lt;/p&gt;
+    &lt;/footer&gt;
+</code></pre></div><p>\u5176\u4E2D\uFF0Cheader\u6807\u7B7E\u5305\u542B\u7684\u662F\u5BFC\u822A\u680F\u7684\u90E8\u5206\uFF0Csection\u6807\u7B7E\u5305\u542B\u7684\u662F\u524D\u7AEF\u6280\u80FD\u56FE\u7684\u90E8\u5206\uFF0Cfooter\u6807\u7B7E\u5305\u542B\u7684\u662F\u9875\u811A\u90E8\u5206\u3002</p><p>\u7136\u540E\u5728index.css\u4E2D\uFF0C\u6DFB\u52A0\u5982\u4E0B\u7684css\u6837\u5F0F\uFF1A</p><div class="language-"><pre><code>* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+button:focus {
+  outline: none;
+}
+a {
+  text-decoration: none;
+  color: white;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+nav {
+  height: 60px;
+  padding: 20px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  background-color:#171717;
+}
+.navbar-header {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+}     
+.navbar-toggle {
+  height: 37px;
+  width: 30%;
+  text-align: center;
+  display: none;
+  border: none;
+  background-color: #171717;
+}
+.navbar-toggle span {
+  display: block;
+  width: 30px;
+  margin: 3px 0;
+  border-bottom: 2px solid #fff;
+}
+.navbar-logo {
+  display: block;
+}
+.navbar-nav li {
+  display: inline-block;
+  padding: 0.5em;
+}
+
+.navbar-nav li  a{
+  border-radius:3px;
+  padding:4px 8px;
+}
+
+.navbar-nav li a:hover{
+  color:#171717;
+  background-color:#fff;
+  box-shadow:1px 1px 1px 0 #fff
+}
+
+.active {
+  color:#171717;
+  background-color:#fff;
+  box-shadow:1px 1px 1px 0 #fff
+}
+.content-wrap {
+  height: calc(100% - 196px);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content-wrap img{
+  height: 100%;
+  width: auto;
+}
+
+.foot-wrap {
+  width: 100%;
+  height: 120px;
+  line-height: 50px;
+  text-align: center;
+  background-color: #695f5f;
+}
+
+@media(max-width: 768px) {
+  nav {
+    display: block;
+    padding: 0px;
+  }
+  .navbar-header {
+    padding: 10px 0px 10px 10px;
+  }     
+  .navbar-toggle {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    width: 30%;
+    text-align: center;
+  }
+  .collapse {
+    display: none;
+  } 
+  .navbar-collapse {
+    height: 100%;
+    width:30%;
+    right: 0;
+    position:fixed;
+    background-color:#171717;
+  }
+  .navbar-collapse li {
+    width: 100%;
+    border-bottom: 1px solid #fff;;
+    text-align: center;
+  }
+}
+</code></pre></div><p>\u6700\u540E\uFF0C\u5728\u5728index.html\u6587\u4EF6\u4E2D\u7684body\u6807\u7B7E\u7684\u5E95\u90E8\u6DFB\u52A0js\u63A7\u5236\u7684\u76F8\u5173\u4EE3\u7801\uFF1A</p><div class="language-"><pre><code> &lt;script&gt;
+      var toggleButton = document.querySelector(&#39;.navbar-toggle&#39;);
+      var collapsedElm = document.querySelector(&#39;.navbar-collapse&#39;);
+      toggleButton.addEventListener(&#39;click&#39;, function() {
+        collapsedElm.classList.toggle(&#39;collapse&#39;);
+      });
+    var navList = document.getElementById(&#39;navList&#39;);
+    var oList=navList.getElementsByTagName(&quot;a&quot;);
+    for(var i=0;i&lt;oList.length;i++){
+        oList[i].onclick=function(){
+            for(var j=0;j&lt;oList.length;j++){
+              oList[j].className=&quot;&quot;;
+            }
+            this.className+=&quot;active&quot;;
+        }
+    }
+ &lt;/script&gt;
+</code></pre></div><h2 id="\u6700\u7EC8\u6548\u679C" tabindex="-1">\u6700\u7EC8\u6548\u679C <a class="header-anchor" href="#\u6700\u7EC8\u6548\u679C" aria-hidden="true">#</a></h2><ul><li><p>pc \u7AEF\u6548\u679C\uFF1A <img src="https://upload-images.jianshu.io/upload_images/11988064-c452d0716bacfce0.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt="Screen Shot 2019-01-06 at 1.47.02 PM.png"></p></li><li><p>h5\u79FB\u52A8\u7AEF\u6548\u679C\uFF1A <img src="https://upload-images.jianshu.io/upload_images/11988064-f729a83e72cb656b.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt="Screen Shot 2019-01-06 at 1.47.28 PM.png"> \u70B9\u51FB\u6298\u53E0\u5BFC\u822A\u680F\u540E\uFF1A <img src="https://upload-images.jianshu.io/upload_images/11988064-f021cb2be8575d9f.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" alt="Screen Shot 2019-01-06 at 1.48.53 PM.png"></p></li></ul>`,28),i=[o];function r(s,c,p,d,g,u){return a(),n("div",null,i)}var v=t(l,[["render",r]]);export{f as __pageData,v as default};
